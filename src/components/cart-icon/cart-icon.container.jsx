31258bf09539b2fd0a4d0d9540dfd6eb8cdfc270
Gus Mendez <gus.mendez.99@gmail.com>
2020-04-07 17:20:01 -0600

    CRWN Store usign GraphQL and Apollo instead of Redux and Firestore

---
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+//'compose' has not been include in new Apollo versions, so...
+import { flowRight } from 'lodash'
+
+import CartIcon from './cart-icon.component';
+
+const TOGGLE_CART_HIDDEN = gql`
+  mutation ToggleCartHidden {
+    toggleCartHidden @client
+  }
+`;
+
+const GET_ITEM_COUNT = gql`
+  {
+    itemCount @client
+  }
+`;
+
+const CartIconContainer = ({ data: { itemCount }, toggleCartHidden }) => (
+  <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
+);
+
+export default flowRight(
+  graphql(TOGGLE_CART_HIDDEN, { name: 'toggleCartHidden' }),
+  graphql(GET_ITEM_COUNT)
+)(CartIconContainer);
